1ad2ef798da1d9785ed6cd8723e5257427a1d5e8
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-08-17 17:37:31 +0300

    test add group extract methods

--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,55 @@
 import unittest
-from selenium.webdriver.chrome.webdriver import WebDriver
 
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
-        self.wd.implicity_wait(60)
+        self.wd.implicitly_wait(60)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("group#1")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("group_header#1")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("group_footer#1")
+        wd.find_element_by_name("submit").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("input[type=submit]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost:8080/addressbook/")
+        
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
 
 
+if __name__ == '__main__':
+    unittest.main()
