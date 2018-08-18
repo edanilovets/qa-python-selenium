4917d78ae5145bcb96a914196e6a7bca0354c571
Evgeniy Danilovets <evgeniy.danilovets@gmail.com>
2018-08-18 11:53:28 +0300

    change test framework to pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,65 +1,23 @@
-import unittest
-
-from selenium.webdriver.chrome.webdriver import WebDriver
+import pytest
 from group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=submit]").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost:80/addressbook/")
-
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="group#1", header="group_header#1", footer="group_footer#1"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group#1", header="group_header#1", footer="group_footer#1"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
+
