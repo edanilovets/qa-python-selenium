eb77fae9c624d09b8561707c865724f5975bcb47
Evgeniy Danilovets <evgeniy.danilovets@gmail.com>
2018-08-19 13:30:32 +0300

    add test for first group deletion

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,18 +2,7 @@
 Application address book
 Tests for adding groups
 """
-
-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_group(app):
@@ -26,4 +15,3 @@ def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
-
