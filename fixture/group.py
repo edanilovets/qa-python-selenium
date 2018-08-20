b5349bcb892cbd67b1c01019d686b28bd57c78f5
Evgeniy Danilovets <evgeniy.danilovets@gmail.com>
2018-08-20 21:21:08 +0300

    finish tests for first group modifying

--- fixture/group.py
+++ fixture/group.py
@@ -6,19 +6,26 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        if group.name is not None:
+            wd.find_element_by_name("group_name").click()
+            wd.find_element_by_name("group_name").clear()
+            wd.find_element_by_name("group_name").send_keys(group.name)
+        if group.header is not None:
+            wd.find_element_by_name("group_header").click()
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(group.header)
+        if group.footer is not None:
+            wd.find_element_by_name("group_footer").click()
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(group.footer)
+
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
@@ -29,8 +36,15 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def modify(self):
-        # todo: write this method
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
     def open_groups_page(self):
         wd = self.app.wd
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,13 +1,18 @@
+"""
+Application address book
+Tests for modifying groups
+"""
+
 from model.group import Group
 
 
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="New group"))
+    app.group.modify_first_group(Group(name="z group#000"))
     app.session.logout()
 
 
 def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(header="New header"))
+    app.group.modify_first_group(Group(header="z header#000"))
     app.session.logout()
