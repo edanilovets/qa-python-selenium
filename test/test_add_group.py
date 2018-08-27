cbd567260a1a03f79a55a72933ce76463854b591
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-08-27 18:33:15 +0300

    add assert in add group test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,10 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group#1", header="group_header#1", footer="group_footer#1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
