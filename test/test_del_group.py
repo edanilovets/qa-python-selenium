3ee18cc834771142dad6ad59cacb7f26625166f2
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-08-27 18:41:37 +0300

    add assert of groups list length to all tests

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -6,6 +6,9 @@ from model.group import Group
 
 
 def test_delete_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="group#for_del", header="group_header#for_del", footer="group_footer#for_del"))
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -7,8 +7,14 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="z group#000"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="z header#000"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
