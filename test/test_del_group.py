d4515c4fd0c7d26519dccfcb5f2b377eca3a57a5
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-08-27 17:55:00 +0300

    pre-conditions for first group deletion test

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,10 @@
 Application address book
 Tests for deletion groups
 """
+from model.group import Group
 
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="group#for_del", header="group_header#for_del", footer="group_footer#for_del"))
     app.group.delete_first_group()
