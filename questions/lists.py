ac9a492c68471aa49df6db7dc20e0927b3767712
Evgeniy Danilovets <evgeniy.danilovets@gmail.com>
2018-08-31 22:57:11 +0300

    learning some stuff about lists

--- questions/lists.py
+++ questions/lists.py
@@ -1,16 +1,11 @@
 from model.group import Group
 
-groups = []
+
 group_1 = Group(gid=1, name="group1", header="header1", footer="footer1")
 group_2 = Group(gid=10, name="group2", header="header2", footer="footer2")
 group_3 = Group(gid=13, name="group3", header="header3", footer="footer3")
 group_4 = Group(gid=100, name="group3", header="header3", footer="footer3")
-
-groups.append(group_1)
-groups.append(group_2)
-groups.append(group_3)
-groups.append(group_1)
-groups.append(group_4)
+groups = [group_1, group_2, group_3, group_4]
 
 # print(groups)
 # groups[0:1] = []
@@ -22,8 +17,16 @@ groups.append(group_4)
 # squares = [x**2 for x in range(10)]
 # print(squares)
 
+# Some tips
+list_int = list(range(0, 100, 4))
+print(list_int)
+
+# Multiple assignment
+cat = ['fat', 'orange', 'loud']
+size, color, noise = cat
+print(size)
+print(color)
+print(noise)
+
 # Questions
 # How to select an element from list?
-print(groups)
-print("Index 1: %s" % groups[1])
-print("Last: %s" % groups[-1])
