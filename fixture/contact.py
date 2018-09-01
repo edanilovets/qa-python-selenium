de1dda8c47a2b719edf7a6505361eeb05f8b05ae
Evgeniy Danilovets <evgeniy.danilovets@gmail.com>
2018-09-01 22:10:07 +0300

    finish phones test

--- fixture/contact.py
+++ fixture/contact.py
@@ -12,14 +12,34 @@ class ContactHelper:
     def get_contacts_list(self):
         wd = self.app.wd
         self.open_home_page()
-        contacts = []   # list of contacts objects
+        contacts = []  # list of contacts objects
         lines = wd.find_elements_by_css_selector("tr[name=entry]")
         for i in range(len(lines)):
             contact = Contact()
-            contact.cid = lines[i].find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")\
+            contact.cid = lines[i].find_elements_by_tag_name("td")[0].find_element_by_tag_name("input") \
                 .get_attribute("value")
             contact.last_name = lines[i].find_elements_by_tag_name("td")[1].text
             contact.first_name = lines[i].find_elements_by_tag_name("td")[2].text
             contact.all_phones = lines[i].find_elements_by_tag_name("td")[5].text
             contacts.append(contact)
+        return contacts
+
+    def get_contacts_from_edit_page(self):
+        wd = self.app.wd
+        self.open_home_page()
+        contacts = []  # list of contacts objects
+        rows = wd.find_elements_by_css_selector("tr[name=entry]")
+        for i in range(len(rows)):
+            lines = wd.find_elements_by_css_selector("tr[name=entry]")
+            contact = Contact()
+            contact.cid = lines[i].find_elements_by_tag_name("td")[0].find_element_by_tag_name("input") \
+                .get_attribute("value")
+            lines[i].find_elements_by_tag_name("td")[7].click()
+            contact.first_name = wd.find_element_by_name("firstname").get_attribute("value")
+            contact.last_name = wd.find_element_by_name("lastname").get_attribute("value")
+            contact.home_phone = wd.find_element_by_name("home").get_attribute("value")
+            contact.mobile_phone = wd.find_element_by_name("mobile").get_attribute("value")
+            contact.work_phone = wd.find_element_by_name("work").get_attribute("value")
+            contacts.append(contact)
+            self.open_home_page()
         return contacts
--- test/test_phones.py
+++ test/test_phones.py
@@ -2,9 +2,17 @@
 Application address book
 Tests for contacts on home page
 """
+import re
 
 
 def test_phones(app):
-    contacts = app.contact.get_contacts_list()
-    # todo: add test implementation here
-    print(contacts)
+    first_contact_home = app.contact.get_contacts_list()[0]
+    first_contact_form = app.contact.get_contacts_from_edit_page()[0]
+
+    def clear(s):
+        return re.sub('[() -]', '', s)
+
+    phones_1 = first_contact_home.all_phones.splitlines()
+    phones_2 = [clear(first_contact_form.home_phone), clear(first_contact_form.mobile_phone),
+                clear(first_contact_form.work_phone)]
+    assert phones_1 == phones_2
