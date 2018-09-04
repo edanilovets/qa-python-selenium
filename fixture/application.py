6ca25c8e4317e666467757c0f956e463f671d20f
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-09-04 15:01:24 +0300

    add options to fixture for enter --browser and --url  from command line

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 class Application:
 
-    def __init__(self, browser):
+    def __init__(self, browser, url):
         if browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "firefox":
@@ -20,10 +20,11 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.url = url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost:8080/addressbook/")
+        wd.get(self.url)
 
     def destroy(self):
         self.wd.quit()
--- test/conftest.py
+++ test/conftest.py
@@ -4,7 +4,9 @@ from fixture.application import Application
 
 @pytest.fixture(scope="session", autouse=True)
 def app(request):
-    fixture = Application(browser="firefox")
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--url")
+    fixture = Application(browser=browser, url=base_url)
     fixture.session.login(username="admin", password="secret")
 
     def fin():
@@ -13,3 +15,8 @@ def app(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--url", action="store", default="http://localhost:8080/addressbook/")
