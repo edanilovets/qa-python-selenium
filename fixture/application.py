c4d8f2041125d7143b587914cb391613d800214a
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-09-04 14:13:25 +0300

    simplify fixture

--- fixture/application.py
+++ fixture/application.py
@@ -1,24 +1,25 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 from fixture.contact import ContactHelper
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+            self.wd = webdriver.Firefox(firefox_binary=binary)
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+
         self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
-    def is_valid(self):
-        try:
-            self.wd.current_url
-            return True
-        except:
-            return False
 
     def open_home_page(self):
         wd = self.wd
--- test/conftest.py
+++ test/conftest.py
@@ -1,24 +1,12 @@
 import pytest
 from fixture.application import Application
 
-fixture = None
-
-
-@pytest.fixture
-def app():
-    global fixture
-    if fixture is None:
-        fixture = Application()
-        fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-            fixture.session.login(username="admin", password="secret")
-    return fixture
-
 
 @pytest.fixture(scope="session", autouse=True)
-def stop(request):
+def app(request):
+    fixture = Application(browser="firefox")
+    fixture.session.login(username="admin", password="secret")
+
     def fin():
         fixture.session.logout()
         fixture.destroy()
